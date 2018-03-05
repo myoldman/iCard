1b04cbbc908c980268a0a4ab905628029f48d318
liuh <50399554@qq.com>
2018-03-05 16:11:14 +0800

    导出图片背景添加
    导出图片背景添加

--- pages/card/create.js
+++ pages/card/create.js
@@ -1,6 +1,6 @@
 //logs.js
 const util = require('../../utils/util.js')
-
+const app = getApp()
 Page({
   data: {
     showView: false,
@@ -9,6 +9,10 @@ Page({
     showShare: false,
     showSave : false,
     sharePath: '',
+    lastX: 0,
+    lastY: 0,
+    text: "没有滑动",
+    currentGesture: 0,
   },
   onLoad: function () {
     this.setData({
@@ -53,11 +57,25 @@ Page({
     const ctx = wx.createCanvasContext('cardCanvas')
     const CANVAS_W = 300
     const CANVAS_H = 375
+    var totalWidth = app.globalData.width
+    var totalHeight = app.globalData.height
+    var bgWidth = app.globalData.bgWidth
+    var bgHeight = app.globalData.bgHeight
+
+    var widthMod =  parseInt(totalWidth / bgWidth) + 2
+    var heightMod = parseInt(totalHeight / bgHeight) + 2
+    console.log(widthMod)
+    console.log(heightMod)
+    for (var j = 0; j < heightMod; j++){
+      for(var i = 0; i< widthMod; i++){
+        ctx.drawImage(app.globalData.bgImages[0], bgWidth * i, bgHeight*j, 56, 28)
+      }
+    }
+    ctx.draw(true)
     ctx.setTextAlign('center')
     ctx.setFontSize(15)
     ctx.fillText(e.detail.value.content, 50, 50)
-    ctx.save()
-    ctx.draw()
+    ctx.draw(true)
     wx.hideLoading()
   },
 
@@ -66,11 +84,9 @@ Page({
     wx.canvasToTempFilePath({
       canvasId: 'cardCanvas',
       complete: function (res) {
-        console.log(res);
+        //console.log(res);
       },
       success: function (res) {
-        console.log("aaaaaaaaa")
-        console.log(res.tempFilePath)
         var filepath = res.tempFilePath
         wx.saveImageToPhotosAlbum({
           filePath: filepath,
@@ -82,5 +98,61 @@ Page({
   },
   drawImage: function(content) {
     
-  }
+  },
+   handletouchmove: function (event) {
+    console.log(event)
+    if (this.data.currentGesture != 0) {
+      return
+    }
+    let currentX = event.touches[0].pageX
+    let currentY = event.touches[0].pageY
+    let tx = currentX - this.data.lastX
+    let ty = currentY - this.data.lastY
+    let text = ""
+    //左右方向滑动
+    if (Math.abs(tx) > Math.abs(ty)) {
+      if (tx < 0) {
+        text = "向左滑动"
+        this.data.currentGesture = 1
+      }
+      else if (tx > 0) {
+        text = "向右滑动"
+        this.data.currentGesture = 2
+      }
+
+    }
+    //上下方向滑动
+    else {
+      if (ty < 0) {
+        text = "向上滑动"
+        this.data.currentGesture = 3
+
+      }
+      else if (ty > 0) {
+        text = "向下滑动"
+        this.data.currentGesture = 4
+      }
+
+    }
+
+    //将当前坐标进行保存以进行下一次计算
+    this.data.lastX = currentX
+    this.data.lastY = currentY
+    this.setData({
+      text: text,
+    });
+  },
+
+  handletouchtart: function (event) {
+    console.log(event)
+    this.data.lastX = event.touches[0].pageX
+    this.data.lastY = event.touches[0].pageY
+  },
+  handletouchend: function (event) {
+    console.log(event)
+    this.data.currentGesture = 0
+    this.setData({
+      text: "没有滑动",
+    });
+  },
 })
