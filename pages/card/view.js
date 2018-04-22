a88c7de3c0a67417cd03cde738b92312b23ceb69
liuh <50399554@qq.com>
2018-04-22 09:17:23 +0800

    添加markdown解析

--- pages/card/view.js
+++ pages/card/view.js
@@ -1,22 +1,30 @@
 // pages/card/view.js
-var WxParse = require('../../wxParse/wxParse.js');
-
+var MdParse = require('../../utils/mdParser.js');
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    currentBgCss: 'main1',
+    bgImgWidth: 0,
+    bgImgHeight: 0,
+    bgImgUrl: '',
+    bgImages: app.globalData.bgImages,
+    currentBgImage: app.globalData.bgImages[0],
+    width: app.globalData.width,
+    height: app.globalData.height,
+    ratio: app.globalData.ratio,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var article = '######## 张大春 谈秋夜 \r\n#张大春曾点评过鲁迅那**两株著名的枣树**：\r\n> 秋夜》篇首这一株还有一株的枣树示范了白话文运动发轫之际的一种独特要求，作者有意识的通过描述程序展现观察程序，为了使作者对世界的观察活动准确无误的复印在读者的心象之中，描述的目的便不只在告诉读者看什么，而是怎么看，**鲁迅奇怪而冗赘的句子不是为了让读者看到两株枣树，而是暗示读者以适当的速度在后园中向墙外转移目光经过一株枣树，再经过一株枣树，然后延展向一片奇怪而高的夜空。**aaaa\r\n他还曾提到过\r\n- 列表不带加粗字体不换行时\r\n- 列表不带加粗字体换行换行换行换行换行换行换行时行换行时行换行时行换行时行换行时行换行时\r\n- 列表带**加粗字体**不换行时*哈哈哈**baaaaab**哈哈哈哈a**嘿嘿嘿1**aaaab';
+    var article = '######## 张大春 谈秋夜 \r\n\r\n#张大春曾点评过鲁迅那**两株著名的枣树**：\r\n\r\n> 秋夜》篇首这一株还有一株的枣树示范了白话文运动发轫之际的一种独特要求，作者有意识的通过描述程序展现观察程序，为了使作者对世界的观察活动准确无误的复印在读者的心象之中，描述的目的便不只在告诉读者看什么，而是怎么看，**鲁迅奇怪而冗赘的句子不是为了让读者看到两株枣树，而是暗示读者以适当的速度在后园中向墙外转移目光经过一株枣树，再经过一株枣树，然后延展向一片奇怪而高的夜空。**aaaa\r\n他还曾提到过\r\n- 列表不带加粗字体不换行时\r\n- 列表不带加粗字体换行换行换行换行换行换行换行时行换行时行换行时行换行时行换行时行换行时\r\n- 列表带**加粗字体**不换行时*哈哈哈**baaaaab**哈哈哈哈a**嘿嘿嘿1**aaaab';
     var that = this;
-    WxParse.wxParse('article', 'md', article, that, 5);
+    MdParse.mdParse('card', article, that, '#FFFFFF', '', '');
   },
 
   /**
---
+++ utils/mdParser.js
@@ -0,0 +1,102 @@
+/**
+ * 配置及公有属性
+ **/
+var realWindowWidth = 0;
+var realWindowHeight = 0;
+var ratio = 0;
+wx.getSystemInfo({
+  success: function (res) {
+    realWindowWidth = res.windowWidth
+    realWindowHeight = res.windowHeight
+    ratio = res.ratio
+  }
+})
+
+/**
+ * 主函数入口区
+ **/
+function mdParse(bindName, data, target, bgColor, bg, bgImg) {
+  var that = target;
+  //存放转化后的数据
+  var transData = genCardData(data)
+  transData.bg = bg
+  transData.bgColor = bgColor
+  transData.bgImg = bgImg
+  var bindData = {};
+  console.log(transData)
+  bindData[bindName] = transData;
+  that.setData(bindData)
+}
+
+function genCardData(content) {
+  var transData = {items:[]}
+  content = content.replace(/\r\n/g, "\n")
+  var contents = content.split("\n")
+  for (var i = 0; i < contents.length; i++) {
+    var item = {blocks:[]}
+    var line = contents[i]
+    var origLine = line
+    var itemType = 'mdNormal'
+    var regTitle = /^#+/
+    var regResult = regTitle.exec(line)
+    // 段落为标题
+    if (regResult) {
+      itemType = 'mdTitle'
+    }
+    
+    if (line.length > 0 && line[0] == '-') {
+      // 段落为列表
+      line = line.replace("-", ""); 
+      itemType = 'mdList'
+    } else if (line.length > 0 && line[0] == '>') {
+      // 段落为引用
+      line = line.replace(">", ""); 
+      itemType = 'mdQuote'
+    }
+
+    var regBold = /(([\*]{2})(.+?)([\*]{2}))/g
+    var boldMatch = line.match(regBold)
+    var boldArr = []
+    if (boldMatch) {
+      for (var k = 0; k < boldMatch.length; k++) {
+        var strMatch = boldMatch[k]
+        var matchIndex = line.indexOf(strMatch)
+        boldArr.push({ start: matchIndex - 4 * k, end: matchIndex + strMatch.length - 4 - 4 * k - 1 })
+      }
+      var lastMatch
+      var classStr = "mdOrange"
+      if (itemType == 'mdList') {
+        classStr = "mdOrange mdBold"
+      }
+      for (var j = 0; j < boldMatch.length; j++) {
+        var strMatch = boldMatch[j]
+        
+        if(j == 0 ){
+          var block = line.substring(0, line.indexOf(strMatch))
+          item.blocks.push({content:block, classStr : ''})
+        } else {
+          var block = line.substring(line.indexOf(lastMatch) + lastMatch.length, line.indexOf(strMatch))
+          item.blocks.push({ content: block, classStr: '' })
+        }
+        item.blocks.push({ content: strMatch.substring(2, strMatch.length - 2), classStr : classStr })
+        //line = line.replace(strMatch, appendTag + strMatch.substring(2, strMatch.length - 2) + "</text>");
+        lastMatch = strMatch
+      }
+    } else {
+      if (line.length == 0) {
+        itemType += " mdEmpty"
+      }
+      
+      item.blocks.push({content: line, classStr:''})
+    }
+    item.type = itemType
+    console.log(item.blocks)
+    transData.items.push(item)
+  }
+  return transData
+}
+
+module.exports = {
+  mdParse: mdParse
+}
+
