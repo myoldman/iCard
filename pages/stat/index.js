36711e203de00ad95c96459ff06fd4c170cd97b4
liuh <50399554@qq.com>
2018-05-03 17:35:29 +0800

    添加日历组件

--- pages/stat/index.js
+++ pages/stat/index.js
@@ -1,4 +1,4 @@
-
+import initCalendar, { getSelectedDay, jumpToToday } from '../../template/calendar/index';
 const util = require('../../utils/util.js')
 
 const app = getApp()
@@ -9,6 +9,34 @@ Page({
   },
 
   onShow: function () {
+    initCalendar({
+      // multi: true, // 是否开启多选,
+      // disablePastDay: true, // 是否禁选过去日期
+      /**
+       * 选择日期后执行的事件
+       * @param { object } currentSelect 当前点击的日期
+       * @param { array } allSelectedDays 选择的所有日期（当mulit为true时，才有allSelectedDays参数）
+       */
+      afterTapDay: (currentSelect, allSelectedDays) => {
+        console.log('当前点击的日期', currentSelect);
+        allSelectedDays && console.log('选择的所有日期', allSelectedDays);
+        console.log('getSelectedDay方法', getSelectedDay());
+      },
+      /**
+       * 日期点击事件（此事件会完全接管点击事件）
+       * @param { object } currentSelect 当前点击的日期
+       * @param { object } event 日期点击事件对象
+       */
+      // onTapDay(currentSelect, event) {
+      //   console.log(currentSelect);
+      //   console.log(event);
+      // },
+    });
   },
-
+  /**
+   * 跳转至今天
+   */
+  jump() {
+    jumpToToday();
+  }
 })
